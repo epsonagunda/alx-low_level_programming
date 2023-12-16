a32553ae0fa24c2c779647d84cf73402d183d4b0
epsonagunda <epsonagunda182@gmail.com>
2023-12-16 08:44:45 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * main - A program that use put function to display a line
+ * return: always 0
+ */
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	
+	return (0);
+}

257b9bd31da044137da717182416ac001fcaa29b
epsonagunda <epsonagunda182@gmail.com>
2023-12-17 23:38:40 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A program to print the size of various type of computers.
+ * Return: 0 (pass)
+ */
+
+int main(void)
+{
+	char 1;
+	int 2;
+	long int 3;
+	long long int 4;
+	float 5;
+
+
+printf("Size of a char : %lu byte(s)\n", (unsigned long)sizeof(1));
+printf("Size of a char : %lu byte(s)\n", (unsigned long)sizeof(2));
+printf("Size of a char : %lu byte(s)\n", (unsigned long)sizeof(3));
+printf("Size of a char : %lu byte(s)\n", (unsigned long)sizeof(4));
+printf("Size of a char : %lu byte(s)\n", (unsigned long)sizeof(5));
+return (0);

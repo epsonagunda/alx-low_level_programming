0a6ac2c4c74e0fdb4704d186449d16f5b41b48e4
epsonagunda <epsonagunda182@gmail.com>
2023-12-18 02:16:01 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /** 
  * main - A program to print line with the standad error.
  * Return: 1(pass)
  */
 int main(void)
 {
-fprintf(stderr,"and that piece of art is useful\" - \Dora Korpar,2015-10-19\n");
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return(1);
 }

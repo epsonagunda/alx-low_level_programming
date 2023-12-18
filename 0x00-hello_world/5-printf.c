54f54f4b4008ed64a70dd43804488fe533cc886f
epsonagunda <epsonagunda182@gmail.com>
2023-12-17 23:09:58 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program to print a new line using printf function.
+ * Return:  0 (pass)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

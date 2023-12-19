fb6446484f841b3244e2dfb6e36a3cb0bc98dcdb
epsonagunda <epsonagunda182@gmail.com>
2023-12-19 07:06:33 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * main - statrting point
+ * Return: 0 (end)
+ */
+void print_alphabet(void)
+{
+	char letter;
+	for (letter = 'a';letter <= 'z';letter++);
+	_putchar('\n')
+}

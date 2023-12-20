ce2adda930d17b33f318b823585ff7d4fbe40253
epsonagunda <epsonagunda182@gmail.com>
2023-12-20 05:28:35 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,12 @@
  */
 int print_last_digit(int n)
 {
-	int last;
-	last = n % 10;
-	if (last < 0)
-	{
-		last = last * -1;
-	}
-	_putchar(last + '0');
-	return (last);
+int last;
+last = n % 10;
+if (last < 0)
+{
+last = last * -1;
+}
+_putchar(last + '0');
+return (last);
 }

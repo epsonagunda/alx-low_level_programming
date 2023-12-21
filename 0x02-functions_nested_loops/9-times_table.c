31f3110aa4393082be1e5730373ea9ffcb2b57a3
epsonagunda <epsonagunda182@gmail.com>
2023-12-20 22:35:10 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,17 +4,18 @@
  * Return: empty output
  */
 void times_table(void)
+
 {
 int x, y, z, u, d;
 for (x = 0; x <= 9; x++)
 {
-for (y = 0; y<= 9; y++)
+for (y = 0; y <= 9; y++)
 {
 z = x * y;
 if (z > 9)
 {
 u = z % 10;
-d = (z - u ) / 10;
+d = (z - u) / 10;
 _putchar(44);
 _putchar(32);
 _putchar(d + '0');
@@ -34,4 +35,3 @@ _putchar(z + '0');
 _putchar('\n');
 }
 }
-

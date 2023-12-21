8da149efb9dea9b776c1293f131da0099ac89e91
epsonagunda <epsonagunda182@gmail.com>
2023-12-21 06:01:43 -0800

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - function to calculate the product of two numbers
+ * @a:  first number
+ * @b:  second number
+ * Return: 0 (pass)
+ */
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}

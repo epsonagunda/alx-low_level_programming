3cc4d53708819d395f03d2fcf8c53bcfba9233f7
epsonagunda <epsonagunda182@gmail.com>
2023-12-21 05:35:41 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - program to check for uppercase
+ * @c: variable text
+ * Return: 0 (pass)
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

2b30ce782be7f6f41ab96d1a4fe86cc44900609b
epsonagunda <epsonagunda182@gmail.com>
2023-12-21 06:25:31 -0800

    the number speak

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - function to check for a digit
+ * Return: 0 (pass)
+ */
+void print_numbers(void)
+{
+int number = 48;
+for (c = 48; c < 58; number++)
+{
+_putchar(number);
+}
+_putchar(10);
+}

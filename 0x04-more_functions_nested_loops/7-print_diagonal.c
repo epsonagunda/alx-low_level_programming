cfaf252254ab96ab686a686945495f3927d47afe
epsonagunda <epsonagunda182@gmail.com>
2023-12-21 23:28:19 -0800

    draw diadonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,15 +1,9 @@
 #include "main.h"
-/** 
-*print_diagonal - function to draw diagonal
-*@n: number of times the character
+/**
+* print_diagonal - function to draw diagonal
+* @n: number of times the character
 */
 void print_diagonal(int n)
-{
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
 {
 int i, j;
 for (i = 0; i < n; i++)
@@ -21,7 +15,6 @@ _putchar('\\');
 else if (j < i)
 _putchar(' ');
 }
-_putchar('\n');
-}
+_putchar('\n')
 }
 }

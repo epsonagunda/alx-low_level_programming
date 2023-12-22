56507c1750b748c945335fef6a36e695d094ae6b
epsonagunda <epsonagunda182@gmail.com>
2023-12-22 00:24:02 -0800

    fizz-buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+/**
+ *  main - program to print the number from 1 to 100
+ *  Return: 0 (pass)
+ */
+
+int main(void)
+{
+int num;
+for  (num = 1; num <= 1000; num++)
+{
+if ((num % 3) == 0 && (num % 5) == 0)
+printf("FizzBuzz");
+else if ((num % 3) == 0)
+printf("Fizz");
+else if ((num % 5) == 0)
+printf("Buzz");
+else
+printf("%d", num);
+if (num == 100)
+continue;
+printf(" ");
+}
+printf("\n");
+return (0);
+}

ea6c2a9a2af01f62bbafaeab83f683361439101b
epsonagunda <epsonagunda182@gmail.com>
2024-01-03 04:52:30 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,22 +6,18 @@
  */
 void puts2(char *str)
 {
-int longi = 0;
-int t = 0;
-char *y = str;
-int o;
-while (*y != '\0')
+int len, i;
+
+len = 0;
+
+while (str[len] != '\0')
 {
-y++;
-longi++;
+len++;
 }
-t = longi - 1;
-for (o = 0; o <= t; o++)
+for (i = 0; i < len; i += 2)
 {
-if (o % 2 == 0)
-{
-_putchar(str[0]);
+_putchar(str[i]);
 }
-}
+
 _putchar('\n');
 }

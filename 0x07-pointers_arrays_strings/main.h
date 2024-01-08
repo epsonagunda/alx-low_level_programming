d07c53979fa3e2566c7d7b4d1a8193f9104d5b4a
epsonagunda <epsonagunda182@gmail.com>
2024-01-08 05:38:31 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
+#include <stdio.h>
+#include <stddef.h>
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);

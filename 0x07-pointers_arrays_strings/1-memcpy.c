888298b555c1c4391146cf2bb297ce21c4f410e5
epsonagunda <epsonagunda182@gmail.com>
2024-01-08 04:46:56 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,7 +10,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 char *csrc =  (char *)src;
 char *cdest = (char *)dest;
-unsigned int i =0;
+unsigned int i = 0;
 for (; i < n; i++)
 cdest[i] = csrc[i];
 return (dest);

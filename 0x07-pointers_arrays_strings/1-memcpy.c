69c53886100347b9e6d92e87ffb1abd30fd4de7c
epsonagunda <epsonagunda182@gmail.com>
2024-01-08 04:44:49 -0800

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - function used to copy a data from src addr to dest addr
+ * @dest: destination address
+ * @src: source address
+ * @n: Number of bytes to copy
+ * Return: a pointer to n
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+char *csrc =  (char *)src;
+char *cdest = (char *)dest;
+unsigned int i =0;
+for (; i < n; i++)
+cdest[i] = csrc[i];
+return (dest);
+}

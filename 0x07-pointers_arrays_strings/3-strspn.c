4e9b1a1e2b1257aa24f576d2a698831b18a836a9
epsonagunda <epsonagunda182@gmail.com>
2024-01-08 05:53:47 -0800

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * int _strspn - count all posible substring of a given string
+ * @s: targeted segment
+ * @accept: accepted byte
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int bytes = 0;
+int i;
+
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (accept[i] == *s)
+{
+bytes++;
+break;
+}
+else if ((accept[i + 1]) == '\0')
+return (bytes);
+}
+s++;
+}
+return (bytes);
+}

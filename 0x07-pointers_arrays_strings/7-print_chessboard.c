f529080b8b99f4abaac2e1fb33d69777ac26b5d4
epsonagunda <epsonagunda182@gmail.com>
2024-01-09 05:56:44 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_chessboard - print chessboard
+ * @a: the board to print
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+i = j = 0;
+while (i < 8)
+{
+j = 0;
+while (j < 8)
+{
+_putchar(a[i][j]);
+j++;
+}
+_putchar('\n');
+i++;
+}
+}

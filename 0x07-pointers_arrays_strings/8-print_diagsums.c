0b019788bc226d52553f48c3e38f2381fd58baa2
epsonagunda <epsonagunda182@gmail.com>
2024-01-09 07:08:54 -0800

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_diagsums - two diagonals of a square matrix
+ * @a: pointer to two diagonal array
+ * @size: diagonal
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sumx, sumy;
+
+	sumx = sumy = 0;
+	for (i = 0; i < (size * size); i += size + 1)
+		sumx += a[i];
+	for (i = size - 1; i < (size * size - 1); i += size - 1)
+		sumy += a[i];
+	printf("%d, %d\n", sumx, sumy);
+}

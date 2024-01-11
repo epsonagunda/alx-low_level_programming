cd182a2353099d36ff2c67206f46a7fe8ddcd91f
epsonagunda <epsonagunda182@gmail.com>
2024-01-11 02:11:24 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - function to return a natural sqrt of a number
+ * @n: number
+ * Return: 0 (pass)
+ */
+int _sqrt_recursion(int n)
+{
+return (sqrt2(n, 1));
+}

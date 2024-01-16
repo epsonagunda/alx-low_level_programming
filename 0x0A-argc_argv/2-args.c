8856e675617f20ca6414a70b6d48210c27dc79ba
epsonagunda <epsonagunda182@gmail.com>
2024-01-16 06:44:05 -0800

    program that prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 /**
- * main - 
- * @argc:
- * @argv:
+ * main - startin point
+ * @argc: number of command line to count
+ * @argv: array of command line argument
  * Return: (0)pass
  */
 int main(int argc, char **argv)
 {
-printf();
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
 return (0);
 }

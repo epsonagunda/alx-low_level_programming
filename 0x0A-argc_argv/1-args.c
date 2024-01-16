5fecbc351de6d2cb98b550940a346024f67af940
epsonagunda <epsonagunda182@gmail.com>
2024-01-16 04:39:40 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - starting point
+ * @argc: interger type argument
+ * @argv: is an array of character pointer
+ * Return: (0) pass
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}

560c355021472735f7001a445291fe59ce4ff83a
epsonagunda <epsonagunda182@gmail.com>
2024-01-23 01:21:54 -0800

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the name of the file followed by a new line
+ * Return: 0(pass)
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}

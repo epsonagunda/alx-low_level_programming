8765a1bb5feeabd53d2a115d8c0144f03f2286a0
epsonagunda <epsonagunda182@gmail.com>
2024-02-02 07:47:43 -0800

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,15 +5,16 @@
  * listint_len - function that returns the number of element
  *               in a linked listint_t list.
  * @h: A pointer to the listint_t list.
- * Return: 0(pas)
+ * Return: the number of elements in a linked list_t
  */
-size_t listint_len(const listint_t *h)
+size_t list_len(const list_t *h)
 {
 size_t nodes = 0;
-while (h)
+const list_node_t *current = h->h;
+while (current != NULL)
 {
 nodes++;
 h = h->next;
 }
-return (nodes);
+return (node);
 }

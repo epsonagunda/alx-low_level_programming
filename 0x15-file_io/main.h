234cc12195c0c5f4886842fd645fed60d3ea1d79
epsonagunda <epsonagunda182@gmail.com>
2024-02-12 22:03:43 -0800

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,8 @@
 #define FILE_IO
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);

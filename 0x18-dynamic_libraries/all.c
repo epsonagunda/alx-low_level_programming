284e5c8c149a9eb8d1544e3e7848780e0f133d4c
epsonagunda <epsonagunda182@gmail.com>
2024-03-25 08:10:19 -0700

    lets call c functions from python

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+/**
+*add - addition of two numbers
+*@a: first int
+*@b: second int
+Return: sum of two number
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+*sub  - subtraction of two numbers
+*@a: first int
+*@b: second int
+*Return: subtraction of two number
+*/
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+*mul - multiplication of two numbers
+*@a: first int
+*@b: second int
+*Return: product of two number
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+*mod - mode of two numbers
+*@a: first int
+*@b: second int
+*Return: mode of two number
+*/
+int mod(int a, int b)
+{
+return (a % b);
+}
+/**
+ * div_s - divid two numbers
+ * @a: fisrt number
+ * @b: second number
+ * Return: division of the number
+ */
+int div_s(int a, int b)
+{
+	return (a / b);
+}

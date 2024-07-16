7b0b90aacc3674124234a23c2ef955092798866d
epsonagunda <epsonagunda182@gmail.com>
2024-07-16 05:54:00 -0700

    searchalgoriths

---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,10 @@
+#0x1E. C - Search Algorithms
+
+
+
+Resources
+Read or watch:
+
+Search algorithm
+Space complexity (1)
+Serach Algorithms video playlist
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef _search_algos_h_
+#define _search_algos_h_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+
+#endif
